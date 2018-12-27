d6f2653bbfcb8fae47e333f95757475a0a373dc5
hedraadel <44077777+hedraadel1@users.noreply.github.com>
2018-12-27 19:36:33 +0200

    Initial commit

---
+++ WebForms/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebForms.Code;
+
+namespace WebForms
+{
+    public partial class _Default : PageBase
+    {
+        // returns breadcrumb node for this page. 
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.Title = "Shopia Web Forms Application";
+            Page.MetaKeywords = "Shopping, Electronic Products, Shopia, ASP.NET Web Forms";
+            Page.MetaDescription = "Shopia Web Forms Application";
+
+            if (!IsPostBack)
+            {
+                // set the selected menu 
+
+                SelectedMenu = "home";
+
+                // register javascript to open popup windows (better use bundling).
+
+                RegisterOpenWindowJavaScript();
+            }
+        }
+    }
+}
